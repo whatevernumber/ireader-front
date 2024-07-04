fc7dc11d70b96c8b80a23205460bf8b32822262d
whatevernumber <achford@mail.ru>
2024-07-04 20:24:34 +0300

    Responsive design header's fixes

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -44,59 +44,57 @@
 <div role="alert" class="alert alert-info h-2 flex">
     <span class="text-xs">На сайте используются cookies.</span>
 </div>
-<header class="flex justify-between items-center gap-x-6 bg-base-200 h-16 pl-2 pr-2 mb-4">
+<header class="flex justify-between items-center bg-base-200 h-16 pl-2 pr-2 mb-4">
     <div class="flex items-center gap-x-4">
         <a href="/" class="hover:text-accent flex items-center">
             <img class="w-[60px] rounded-full" src="/img/logo.jpg" alt="Логотип с читающим котиками">
             <p class="ml-2 hidden sm:inline">iBook</p>
         </a>
         <a class="btn btn-xs" on:click={changeTheme}>
-            <span class="hidden sm:inline">Сменить тему</span>
+            <span class="hidden lg:inline">Сменить тему</span>
             <img src="/img/svg/paint-bucket.svg" class="w-4"/>
         </a>
     </div>
 {#if showAuth}
     {#if loggedIn}
-        <div class="flex gap-x-8 justify-between items-center ml-8">
-        {#if $page.url.pathname !== '/'}
-            <div class="hidden sm:block">
-                <Search type="header" />
-            </div>
-            {/if}
-            <div class="flex flex-col items-center">
-                <nav class="flex gap-x-5">
-                    {#if $user.currentUser && $user.currentUser.data?.admin}
-                        <a href="/admin/books/add" class="btn btn-xs">
-                            <span class="hidden sm:inline">Добавить книгу</span>
-                            <img src="/img/svg/book-add.svg" class="w-4"/>
-                        </a>
-                    {/if}
-                    <a class="btn btn-accent btn-xs" href='/books/favs'>
-                        <span class="hidden sm:inline">Избранное</span>
-                        <img src="/img/svg/star.svg" class="w-4"/>
-                    </a>
-                    <a class="btn btn-secondary btn-xs" href='/books/completed'>
-                        <span class="hidden sm:inline">Прочитано</span>
-                        <img src="/img/svg/book-check-trans.svg" class="w-4"/>
-                    </a>
-                    <a class="btn btn-primary btn-xs" href={'/books/progress'}>
-                        <span class="hidden sm:inline">На чтении</span>
-                        <img src="/img/svg/book-open.svg" class="w-4"/>
-                        {#if $user.progress.data && $user.progress.data.length}
-                        <span class="hidden sm:inline">{$user.progress.data.length}</span>
-                        {/if}
-                    </a>
-                    <a class="btn btn-xs" href={`/user/${$user.currentUser.data.id}`}>
-                        <span class="hidden sm:inline">Профиль</span>
-                        <img src={$user.currentUser.data.avatar ? PUBLIC_AVATARS + $user.currentUser.data.avatar : '/img/svg/book-user.svg'} class="w-4"/>
-                    </a>
-                    <a class="btn btn-xs btn-error" on:click={logout}>
-                        <span class="hidden sm:inline">Выйти</span>
-                        <img src="/img/svg/leave-icon.svg" class="w-4"/>
-                    </a>
-                </nav>
-            </div>
+    {#if $page.url.pathname !== '/'}
+        <div class="hidden sm:block">
+            <Search type="header" />
         </div>
+    {/if}
+    <div class="flex flex-col items-center">
+        <nav class="flex gap-x-5">
+            {#if $user.currentUser && $user.currentUser.data?.admin}
+                <a href="/admin/books/add" class="btn btn-xs">
+                    <span class="hidden lg:inline">Добавить книгу</span>
+                    <img src="/img/svg/book-add.svg" class="w-4"/>
+                </a>
+            {/if}
+            <a class="btn btn-accent btn-xs" href='/books/favs'>
+                <span class="hidden lg:inline">Избранное</span>
+                <img src="/img/svg/star.svg" class="w-4"/>
+            </a>
+            <a class="btn btn-secondary btn-xs" href='/books/completed'>
+                <span class="hidden lg:inline">Прочитано</span>
+                <img src="/img/svg/book-check-trans.svg" class="w-4"/>
+            </a>
+            <a class="btn btn-primary btn-xs" href={'/books/progress'}>
+                <span class="hidden lg:inline">На чтении</span>
+                <img src="/img/svg/book-open.svg" class="w-4"/>
+                {#if $user.progress.data && $user.progress.data.length}
+                <span class="hidden lg:inline">{$user.progress.data.length}</span>
+                {/if}
+            </a>
+            <a class="btn btn-xs" href={`/user/${$user.currentUser.data.id}`}>
+                <span class="hidden lg:inline">Профиль</span>
+                <img src={$user.currentUser.data.avatar ? PUBLIC_AVATARS + $user.currentUser.data.avatar : '/img/svg/book-user.svg'} class="w-4"/>
+            </a>
+            <a class="btn btn-xs btn-error" on:click={logout}>
+                <span class="hidden lg:inline">Выйти</span>
+                <img src="/img/svg/leave-icon.svg" class="w-4"/>
+            </a>
+        </nav>
+    </div>
     {:else}
         <div class="flex gap-x-2 ml-auto pr-4">
             <button class="btn btn-accent btn-sm" on:click={() => login_modal.showModal()}>Войти</button>
